3353759c7c65675652ad45f7758054dc21c52393
Lukeses <lukeses09@gmail.com>
2016-03-18 04:56:40 +0800

    Added -Creating Client Record -Deleting Client Record -Populating Client Record -Validating Client Record upon Committing

--- controller/client/main.js
+++ controller/client/main.js
@@ -1,10 +1,10 @@
-//	populate_table_main();
+	populate_table_main();
 
 
 
 
   var table_main = $('#table_main').dataTable({
-    "aoColumnDefs": [ { "bSortable": false, "aTargets": [3] } ],
+    "aoColumnDefs": [ { "bSortable": false, "aTargets": [4,5] } ],
     "aaSorting": []
   });  //Initialize the datatable
 
@@ -12,20 +12,19 @@ function populate_table_main(){
 	//ajax now
 	$.ajax ({
 	  type: "POST",
-	  url: "../../model/users/populate_table_main.php",
+	  url: "../../model/clients/populate_table_main.php",
 	  dataType: 'json',      
 	  cache: false,
 	  success: function(s)
 	  {
 	    table_main.fnClearTable();      
-	    var enability='enabled';  
 	    for(var i = 0; i < s.length; i++) 
 	    { 
-	    	if(s[i][2]=='inactive'){enability='disabled'}
 	      table_main.fnAddData
 	      ([
-	        s[i][0],s[i][1],s[i][2],
-	        '<button data-toggle="tooltip" onclick="user_row_del(this.value)" value='+s[i][0]+' data-toggle="modal" class="btn btn-xs  btn-danger" title="Delete" '+enability+'> <i class="fa fa-trash"></i> Delete </button>',      
+	        s[i][1],s[i][2],s[i][3],s[i][4],
+	        '<button data-toggle="tooltip" onclick="client_row_view(this.value)" value='+s[i][0]+' data-toggle="modal" class="btn btn-xs " title="VIEW /Edit"> <i class="fa fa-eye"></i></button>',      	        
+	        '<button data-toggle="tooltip" onclick="client_row_del(this.value)" value='+s[i][0]+' data-toggle="modal" class="btn btn-xs  btn-danger" title="Delete"> <i class="fa fa-trash"></i> </button>',      
 	      ],false); 
 	      table_main.fnDraw();
 
@@ -63,50 +62,98 @@ function reset(){
 function validate_form(){
 	err = false;
 
-	if($('#f_user_name').val()==''){
+	if($('#f_name').val()==''){
 		err = true;
-		$('#f_user_name_div').addClass('has-error');
+		$('#f_name_div').addClass('has-error');
 	}
 	else
-		$('#f_user_name_div').removeClass('has-error');		
+		$('#f_name_div').removeClass('has-error');		
 
-	if($('#f_user_password').val()==''){
+	if($('#f_contact').val()==''){
 		err = true;
-		$('#f_user_password_div').addClass('has-error');
+		$('#f_contact_div').addClass('has-error');
 	}
 	else
-		$('#f_user_password_div').removeClass('has-error');		
+		$('#f_contact_div').removeClass('has-error');	
 
-	if($('#f_user_type').val()=='none'){
+	if($('#f_bdate').val()==''){
 		err = true;
-		$('#f_user_type_div').addClass('has-error');
+		$('#f_bdate_div').addClass('has-error');
 	}
 	else
-		$('#f_user_type_div').removeClass('has-error');	
+		$('#f_bdate_div').removeClass('has-error');	
+
+	if($('#f_gender').val()=='none'){
+		err = true;
+		$('#f_gender_div').addClass('has-error');
+	}
+	else
+		$('#f_gender_div').removeClass('has-error');	
+
+	if($('#f_address').val()==''){
+		err = true;
+		$('#f_address_div').addClass('has-error');
+	}
+	else
+		$('#f_address_div').removeClass('has-error');	
+
+	if($('#f_job').val()==''){
+		err = true;
+		$('#f_job_div').addClass('has-error');
+	}
+	else
+		$('#f_job_div').removeClass('has-error');	
+
+	if( $('#f_mstatus').val()=='married' ){
+		if($('#f_spouse').val()==''){
+			err = true;
+			$('#f_spouse_div').addClass('has-error');
+		}
+		else
+			$('#f_spouse_div').removeClass('has-error');	
+
+		if($('#f_dependents').val()=='' || $('#f_dependents').val()<0){
+			err = true;
+			$('#f_dependents_div').addClass('has-error');
+		}
+		else
+			$('#f_dependents_div').removeClass('has-error');	
+
+	}
+
+
 
 	return err;				
 }
 
 function showSpouse(get){
+	$('#f_spouse_div').removeClass('has-error');     
+	$('#f_dependents_div').removeClass('has-error');  	
 	if(get=='married'){$('#spouse_div').css('display','block');}
 	else{ $('#spouse_div').css('display','none'); }
 }
 
-function user_row_del(id){
-	//ajax now
-	$.ajax ({
-	  type: "POST",
-	  url: "../../model/users/delete.php",
-	  data: 'id='+id,
-	  dataType: 'json',      
-	  cache: false,
-	  success: function(s){		  	
-	  	alert('Success: Deleted ');
-	  	reset();
-	  	populate_table_main();
-	  }  
-	}); 
-	//ajax end  
+function client_row_del(id){
+
+  var choice = confirm("Are you sure you want to Delete?");
+  if(choice==true){
+  			//ajax now
+			$.ajax ({
+			  type: "POST",
+			  url: "../../model/clients/delete.php",
+			  data: 'id='+id,
+			  dataType: 'json',      
+			  cache: false,
+			  success: function(s){		  	
+			  	alert('Success: Deleted ');
+			  	reset();
+			  	populate_table_main();
+			  }  
+			}); 
+  }
+
+
+		
 }
 
 $('#btn_reset').click(function(){ reset(); })
@@ -116,15 +163,24 @@ $('#btn_save').click(function(){
 	if(validate_form()==true){}
 	else{
 
-		var user_name = $('#f_user_name').val();
-		var user_password = $('#f_user_password').val();
-		var user_type = $('#f_user_type').val();	
-		var dataString = 'user_name='+user_name+'&user_password='+user_password+'&user_type='+user_type;
+		var client_name = $('#f_name').val();
+		var client_contact = $('#f_contact').val();
+		var client_bdate = $('#f_bdate').val();
+		var client_gender = $('#f_gender').val();
+		var client_mstatus = $('#f_mstatus').val();
+		var client_address = $('#f_address').val();
+		var client_job = $('#f_job').val();
+		var client_spouse = $('#f_spouse').val();
+		var client_dependents = $('#f_dependents').val();
+		var dataString = 'client_name='+client_name+'&client_contact='+client_contact+'&client_bdate='+client_bdate;
+		dataString+='&client_gender='+client_gender+'&client_mstatus='+client_mstatus+'&client_address='+client_address;
+		dataString+='&client_job='+client_job+'&client_spouse='+client_spouse+'&client_dependents='+client_dependents;
+
 		if(this.value!='update'){ //CREATE MODE
 			//ajax now
 			$.ajax ({
 			  type: "POST",
-			  url: "../../model/users/create.php",
+			  url: "../../model/clients/create.php",
 			  data: dataString,
 			  dataType: 'json',      
 			  cache: false,
